777eb64aba0c10c411e04afc12063303acc4fa51
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-04-04 15:02:02 -0300

    chore(backend): create nest modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
+import { DbModule } from './db/db.module'
+import { ProjectsModule } from './projects/projects.module'
+import { TechnologiesModule } from './technologies/technologies.module'
 
 @Module({
-  imports: [],
+  imports: [DbModule, ProjectsModule, TechnologiesModule],
   controllers: [AppController],
   providers: [],
 })
